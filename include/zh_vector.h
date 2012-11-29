2b4d0ae272681e868f0aa22dc6b4071c3cf7c43b
Menglong Tan <tanmenglong@gmail.com>
2012-11-29 10:27:21 +0800

    u

---
+++ include/zh_vector.h
@@ -0,0 +1,30 @@
+/* -*- encoding: utf-8; indent-tabs-mode: nil -*- */
+
+/***************************************************************
+ *
+ * Copyright (c) 2012, Tan Menglong <tanmenglong@gmail.com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GPL licence
+ *
+ **************************************************************/
+
+#ifndef _ZH_VECTOR_H_
+#define _ZH_VECTOR_H_
+
+/**
+ * 
+ *
+ * @file zh_vector.h
+ * @author Tan Menglong <tanmenglong@gmail.com>
+ * @date Wed Nov 28 18:00:42 2012
+ *
+ **/
+
+#include "zh_public.h"
+
+
+
+#endif /* _ZH_VECTOR_H_ */
+
+/* vim: set expandtab shiftwidth=4 tabstop=4: */
