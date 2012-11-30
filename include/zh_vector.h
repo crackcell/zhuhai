980aa235f860a29f46746960d3ccaf6b2315e933
Menglong Tan <tanmenglong@gmail.com>
2012-12-01 00:41:36 +0800

    u

--- include/zh_vector.h
+++ include/zh_vector.h
@@ -17,13 +17,40 @@
  *
  * @file zh_vector.h
  * @author Tan Menglong <tanmenglong@gmail.com>
- * @date Wed Nov 28 18:00:42 2012
+ * @date Thu Nov 29 10:36:35 2012
  *
  **/
 
+#include <stdlib.h>
+
 #include "zh_public.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+struct zh_vector_slot {
+    size_t size;
+    void *ptr;
+    struct zh_vector_slot *next_slot;
+};
+
+typedef struct zh_vector {
+    int node_count;
+    int slot_count;
+    struct zh_vector_slot *slots;
+} zh_vector_t;
+
+zh_vector_t *zh_vector_alloc();
+
+int zh_vector_free(zh_vector_t *ptr);
+
+int zh_vector_push_front(zh_vector_t *ptr, void *val, size_t size);
+int zh_vector_push_back(zh_vector_t *ptr, void *val, size_t size);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _ZH_VECTOR_H_ */
 
